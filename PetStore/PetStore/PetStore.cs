1971c215d0f1a8a3ba0b31791384ac7fc942d4ce
drooraleigh <drooraleigh@gmail.com>
2023-08-01 17:57:47 -0400

    updating to week 3 assignment

--- PetStore/PetStore/PetStore.cs
+++ PetStore/PetStore/PetStore.cs
@@ -6,6 +6,8 @@ namespace PetStore
     {
         static void Main(string[] args)
         {
+            ProductLogic logic = new ProductLogic();
+
             Console.WriteLine("Press 1 to add a product");
             Console.WriteLine("Type 'exit' to quit");
             string? userInput = Console.ReadLine();
@@ -33,10 +35,21 @@ namespace PetStore
                     Console.Write("Enter length in inches: ");
                     leash.lengthInches = int.Parse (Console.ReadLine());
 
-                    Console.WriteLine(String.Format("Name: {0}\nPrice: {1}\nQuantity: {2}\nDescription: {3}\nMaterial: {4}\nLength in inches: {5}", 
-                        leash.Name, leash.Price, leash.Quantity, leash.Description, leash.Material, leash.lengthInches));
+                    logic.AddProduct(leash);
+                    Console.WriteLine("New product has been added.");
+                    //Console.WriteLine(String.Format("Name: {0}\nPrice: {1}\nQuantity: {2}\nDescription: {3}\nMaterial: {4}\nLength in inches: {5}", 
+                    //leash.Name, leash.Price, leash.Quantity, leash.Description, leash.Material, leash.lengthInches));
+                } else if (userInput.ToLower() == "2")
+                {
+                    Console.WriteLine("Name a product you would like to view.");
+                    userInput = Console.ReadLine();
+                    DogLeash leash = logic.GetDogLeashByName(userInput);
+
+                    Console.WriteLine(String.Format("Name: {0}\nPrice: {1}\nQuantity: {2}\nDescription: {3}\nMaterial: {4}\nLength in inches: {5}",
+                    leash.Name, leash.Price, leash.Quantity, leash.Description, leash.Material, leash.lengthInches));
                 }
                 Console.WriteLine("Press 1 to add a product");
+                Console.WriteLine("Press 2 to view a product");
                 Console.WriteLine("Type 'exit' to quit");
                 userInput = Console.ReadLine();
             }
---
+++ PetStore/PetStore/ProductLogic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    public class ProductLogic
+    {
+        private List<Product> _products;
+        private Dictionary<string, DogLeash> dictDog;
+        private Dictionary<string, CatFood> dictCat;
+        
+        public ProductLogic() 
+        {
+            _products = new List<Product>();
+            dictDog = new Dictionary<string, DogLeash>();
+            dictCat = new Dictionary<string, CatFood>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            if (product is DogLeash)
+            {
+                //add to dictDog
+                dictDog.Add(product.Name, product as DogLeash);
+            } else if (product is CatFood)
+            {
+                //add to dictCat
+                dictCat.Add(product.Name, product as CatFood);
+            }
+            _products.Add(product); 
+        }
+
+        public DogLeash GetDogLeashByName(string name)
+        {
+            return dictDog[name];
+        }
+
+
+        public List<Product> GetAllProducts() 
+        {
+            return _products;
+        }
+    }
+}
